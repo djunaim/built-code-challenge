5015b553a315c17266c6f25bf1161c094c145d62
Monica Djunaidi <modjun12@gmail.com>
2020-09-22 21:37:21 -0500

    added redux logger

--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -1,10 +1,13 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 //TODO: import reducer
 import { architectReducer } from './architect/architectReducer';
+
+const logger = createLogger();
 
 // contain all reducers
 export const rootReducer = combineReducers({ architectReducer })
 
 export type AppState = ReturnType<typeof rootReducer>;
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(logger))
